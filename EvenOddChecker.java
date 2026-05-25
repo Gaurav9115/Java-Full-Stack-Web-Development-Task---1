22753dd9de17fe73930cbba0c2fe63d8af718cf1
Gaurav9115 <gauravrajpootsd87@gmail.com>
2026-05-25 21:59:55 +0530

    Add files via upload

---
+++ EvenOddChecker.java
@@ -0,0 +1,7 @@
+// 3. conditional statements (if - else)
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        int number = 29;
+        System.out.println(number % 2 == 0 ? "Even" : "Odd");
+    }
+}
